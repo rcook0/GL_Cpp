3b7ebeec74ccbd6bc578fd1d51b23d42d8858ef2
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-09-25 13:59:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include "Point2D.hpp"
 #include "Point3D.hpp"
+#include "Line2D.hpp"
+#include "Line3D.hpp"
+#include "Polygon3D.hpp"
 #include <iostream>
 
 int main() {
+    Polygon3D tri({
+        Point3D(0,0,0),
+        Point3D(1,0,0),
+        Point3D(0,1,0)
+    });
+    
     Point2D a(1,2), b(3,4);
     std::cout << "2D add: " << (a+b) << "\n";
     std::cout << "2D dist: " << a.distance_to(b) << "\n";
@@ -11,5 +20,16 @@ int main() {
     std::cout << "3D cross: " << p.cross(q) << "\n";
     std::cout << "3D dot: " << p.dot(q) << "\n";
 
+    Line2D l2(Point2D(0,0), Point2D(3,4));
+    std::cout << "Line2D: " << l2 << " length=" << l2.length() << "\n";
+
+    Line3D l3(Point3D(0,0,0), Point3D(0,3,4));
+    std::cout << "Line3D: " << l3 << " length=" << l3.length() << "\n";
+
+    std::cout << tri << "\n";
+    std::cout << "Centroid: " << tri.centroid() << "\n";
+    std::cout << "Normal: " << tri.normal() << "\n";
+    std::cout << "Area: " << tri.area() << "\n";
+    
     return 0;
 }
