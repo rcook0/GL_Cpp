fd7d2c8c3fcb370a1130ac6ca5140851e1e93a13
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-09-25 15:11:50 +0200

    Update GO_Polygon2D.cpp

--- test/GO_Polygon2D.cpp
+++ test/GO_Polygon2D.cpp
@@ -1,13 +1,26 @@
-#pragma once
-#include "IGraphicObject2D.hpp"
+#include "RasterBuffer.hpp"
 #include "Drawing2D.hpp"
-#include <vector>
+#include "GO_Polygon2D.hpp"
+#include <cmath>
 
-class GO_Polygon2D : public IGraphicObject2D {
-public:
-    std::vector<Point2D> pts;
-    explicit GO_Polygon2D(std::vector<Point2D> pts): pts(std::move(pts)) {}
-    void draw(RasterBuffer<uint8_t>& rb, uint8_t intensity=255) const override {
-        Drawing2D d(rb); d.polygon(pts, intensity);
-    }
-};
+int main() {
+    RasterBuffer<uint8_t> rb(256, 256, 1, 0);
+    Drawing2D d(rb);
+
+    // Axes
+    d.line({0,128}, {255,128}, 128);
+    d.line({128,0}, {128,255}, 128);
+
+    // Circle
+    d.circle({128,128}, 60, 200);
+
+    // Cubic Bezier
+    d.cubic_bezier({20,200},{64,20},{192,236},{236,56}, 64, 255);
+
+    // Triangle polygon
+    GO_Polygon2D tri({{30,30},{120,50},{60,120}});
+    tri.draw(rb, 255);
+
+    rb.save_ppm("out.ppm");
+    return 0;
+}
