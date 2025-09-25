3b7a2409fcce993ebdcfa52830f7dffd519cc114
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-09-25 14:14:17 +0200

    Create Scene3D.cpp

---
+++ src/Scene3D.cpp
@@ -0,0 +1,32 @@
+#include "Scene3D.hpp"
+
+Scene3D::Scene3D(const Projection3D& proj) : projection(proj) {}
+
+void Scene3D::add_object(const Polygon3D& poly) {
+    objects.push_back(poly);
+}
+
+void Scene3D::apply_transformation(const Transformation3D& t) {
+    for (auto& poly : objects) {
+        for (auto& v : poly.vertices) {
+            v = t.apply(v);
+        }
+    }
+}
+
+std::vector<std::vector<Point2D>> Scene3D::project_to_2d() const {
+    std::vector<std::vector<Point2D>> projected;
+    for (const auto& poly : objects) {
+        std::vector<Point2D> verts2d;
+        for (const auto& v : poly.vertices) {
+            verts2d.push_back(projection.project(v));
+        }
+        projected.push_back(verts2d);
+    }
+    return projected;
+}
+
+std::ostream& operator<<(std::ostream& os, const Scene3D& scene) {
+    os << "Scene3D with " << scene.objects.size() << " objects";
+    return os;
+}
